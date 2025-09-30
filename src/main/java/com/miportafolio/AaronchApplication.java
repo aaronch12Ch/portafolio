5ab149423d1760a037f3e7cd38504ad7d3c14264
Aaron Cordova <aron2014123456@gmail.com>
2025-09-30 17:48:48 -0600

    primer commit

---
+++ src/main/java/com/miportafolio/AaronchApplication.java
@@ -0,0 +1,13 @@
+package com.miportafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaronchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaronchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miportafolio/AaronchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miportafolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaronchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
