a5e2075f4fb46bc87bfb40edf0c2b810b789695f
Aaron Cordova <aron2014123456@gmail.com>
2025-10-03 12:44:16 -0600

    solo por seguridad

--- src/main/java/com/miportafolio/model/Proyectos.java
+++ src/main/java/com/miportafolio/model/Proyectos.java
@@ -2,6 +2,7 @@ package com.miportafolio.model;
 
 import jakarta.persistence.*;
 
+import jakarta.validation.constraints.Max;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,11 +19,14 @@ public class Proyectos {
     @Column(length = 20, nullable = false)
     private String nombreProyecto;
 
-    @Column(length = 20, nullable = false)
+    @Column(length = 200, nullable = false)
     private String url;
 
-    @Column(length = 20, nullable = false)
+    @Column(length = 100, nullable = false)
     private String urlImagen;
+
+    @Column(length = 100, nullable = false)
+    private String descripcionProyecto;
 
     @Column(nullable = false)
     private boolean disponibleProyecto;
